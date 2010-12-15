49d3eec6b12fe7c270d5ba51c0676a008b75fb9c
Tomasz Zdanowski <tomasz@amilo.(none)>
2010-12-15 15:58:43 +0100

    first import

---
+++ config/sfWindguruPluginConfiguration.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * sfWindguruPlugin configuration.
+ * 
+ * @package     sfWindguruPlugin
+ * @subpackage  config
+ * @author      Your name here
+ * @version     SVN: $Id: PluginConfiguration.class.php 17207 2009-04-10 15:36:26Z Kris.Wallsmith $
+ */
+class sfWindguruPluginConfiguration extends sfPluginConfiguration
+{
+  const VERSION = '1.0.1';
+
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}
